2d1181dda1c7a56e51d4df693fa5a05961b3ec0e
Jasmine <jsjohn026@gmail.com>
2019-05-03 15:30:10 -0700

    complete phase 2

--- pocket_projects_vanilla_dom/src/drop_down.js
+++ pocket_projects_vanilla_dom/src/drop_down.js
@@ -1,3 +1,4 @@
+import { htmlGenerator } from "./warmup";
 
 const dogs = {
   "Corgi": "https://www.akc.org/dog-breeds/cardigan-welsh-corgi/",
@@ -9,3 +10,45 @@ const dogs = {
   "French Bulldog": "https://www.akc.org/dog-breeds/french-bulldog/" 
 };
 
+const dogLinkCreator = (dogs) => {
+  const dogLinks = [];
+  const dogNames = Object.keys(dogs);
+  
+  dogNames.forEach(dogName => {
+    const dogLink = document.createElement("a");
+    dogLink.innerHTML = dogName;
+    dogLink.href = dogs[dogName];
+
+    const liTag = document.createElement("li");
+    liTag.classList.add("dog-link");
+    liTag.appendChild(dogLink);
+    
+    dogLinks.push(liTag);
+  });
+  // debugger
+  return dogLinks;
+};
+
+const attachDogLinks = () => {
+  const dogLinks = dogLinkCreator(dogs);
+  const dropdownUl = document.querySelector(".drop-down-dog-list");
+  dogLinks.forEach(link => dropdownUl.appendChild(link));
+};
+
+attachDogLinks();
+
+const handleEnter = () => {
+  const dogLinks = document.querySelectorAll(".dog-link");
+  dogLinks.forEach(link => link.classList.add("open"));
+};
+
+const handleLeave = () => {
+  const dogLinks = document.querySelectorAll(".dog-link");
+  dogLinks.forEach(link => link.classList.remove("open"));
+};
+
+const dropdown = document.querySelector(".drop-down-dog-nav");
+// dropdown.addEventListener("mouseenter", handleEnter());
+// dropdown.addEventListener("mouseleave", handleLeave());
+dropdown.addEventListener("mouseenter", handleEnter);
+dropdown.addEventListener("mouseleave", handleLeave);
--- pocket_projects_vanilla_dom/src/warmup.js
+++ pocket_projects_vanilla_dom/src/warmup.js
@@ -2,7 +2,13 @@
 const partyHeader = document.getElementById('party');
 
 export const htmlGenerator = (string, htmlElement) => {
-
+  if (htmlElement.hasChildNodes()) {
+    Array.from(htmlElement.children).forEach((child) => 
+    htmlElement.removeChild(child));
+  }
+  let pTag = document.createElement("p");
+  pTag.innerHTML = string;
+  htmlElement.appendChild(pTag);
 };
 
-htmlGenerator('Party Time.', partyHeader);
+// htmlGenerator('Party Time.', partyHeader);
